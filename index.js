4a358bdfc88286b19bc887e108c23db74b2cd58b
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2024-11-13 20:37:10 -0300

    changing messages structure and exceptions, adding a new endpoint to search bagmons

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dex = require('./data/dex.json');
 const extinctionLevels = require('./data/extinctionLevels.json');
 const info = require('./data/info.json');
 let types = require('./data/types.json');
-const errorMessages = require('./errorMessages.json');
+const messages = require('./data/messages.json');
 
 const app = express();
 const port = 3000;
@@ -24,7 +24,7 @@ app.get('/api/types', (req, res) => {
         if (type)
             return res.status(200).json(type);
         else
-            return res.status(404).json({ error: errorMessages.typeNotFound });
+            return res.status(404).json(messages.typeNotFound);
     }
 
     if (req.query.weaknesses) {
@@ -35,12 +35,37 @@ app.get('/api/types', (req, res) => {
         );
 
         if (filteredTypes.length === 0)
-            return res.status(404).json({ error: errorMessages.weaknessesNotFound });
+            return res.status(404).json(messages.weaknessesNotFound);
         else
             return res.status(200).json(filteredTypes.sort((a, b) => a.id - b.id));
     }
 
     return res.status(200).json(types.sort((a, b) => a.id - b.id));
+});
+
+app.get('/api/dex', (req, res) => {
+
+    if (req.query.id) {
+        const bagmonList = dex.find(bagmon => bagmon.id == req.query.id);
+        if (bagmonList)
+            return res.status(200).json(bagmonList);
+        else
+            return res.status(404).json(messages.bagmonNotFound);
+    }
+
+    if (req.query.starter) {
+        if (req.query.starter !== 'true' && req.query.starter !== 'false')
+            return res.status(404).json(messages.starterValueInvalid);
+
+        const bagmonList = dex.filter(bagmon => bagmon.starter === (req.query.starter === 'true'));
+
+        if (bagmonList)
+            return res.status(200).json(bagmonList);
+        else
+            return res.status(404).json(messages.starterValueInvalid);
+    }
+
+    return res.status(200).json(dex.sort((a, b) => a.id - b.id));
 });
 
 app.listen(port, () => {
